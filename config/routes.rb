37d6e8a1a6bc55e265b758c93f68dc88cf279a77
chase-stevens <chasestevenspersonal@gmail.com>
2019-01-14 17:41:19 -0700

    added nav links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   resources :urls, param: :short
-  get '/:short', to: 'urls#show'
-  get '/:short/goto', to: 'urls#goto'
+  get '/:short', to: 'urls#goto'
 
   root 'home#index'
 end
